c7eb5f0ee6832361f7348db189b33733cb8c74b1
Fernando <iie.fercer@gmail.com>
2026-01-02 12:58:13 -0500

    Moved format_roi to _utils.py

--- zarrdataset/_zarrdataset.py
+++ zarrdataset/_zarrdataset.py
@@ -7,7 +7,7 @@ import random
 import zarr
 import numpy as np
 
-from ._utils import parse_metadata
+from ._utils import parse_metadata, format_roi
 from ._imageloaders import ImageCollection
 from ._samplers import PatchSampler
 
@@ -48,59 +48,6 @@ except ModuleNotFoundError:
                     'still work as a python iterator')
     IterableDataset = object
     PYTORCH_SUPPORT = False
-
-
-def format_roi(roi: Union[slice, Iterable[slice], None]) -> str:
-    """Convert a ROI (slice or tuple of slices) to string format.
-    
-    Parameters
-    ----------
-    roi : Union[slice, Iterable[slice], None]
-        A slice or tuple of slices representing the region of interest.
-    
-    Returns
-    -------
-    str
-        String representation in format "(start_coords):(lengths)"
-        e.g., "(0,0,0):(100,100,3)" or ":" for full slice
-    """
-    if roi is None:
-        return ":"
-    
-    if isinstance(roi, slice):
-        roi = (roi,)
-    
-    if not isinstance(roi, (list, tuple)):
-        return str(roi)
-    
-    # Check if all slices are None (full selection)
-    if all(s.start is None and s.stop is None for s in roi if isinstance(s, slice)):
-        return ":"
-    
-    start_coords = []
-    lengths = []
-    
-    for s in roi:
-        if isinstance(s, slice):
-            start = s.start if s.start is not None else 0
-            stop = s.stop
-            
-            start_coords.append(str(start))
-            
-            if stop is None:
-                lengths.append("-1")  # -1 indicates till end
-            else:
-                length = stop - start
-                lengths.append(str(length))
-        else:
-            # If not a slice, just use as-is
-            start_coords.append(str(s))
-            lengths.append("1")
-    
-    start_str = "(" + ",".join(start_coords) + ")"
-    length_str = "(" + ",".join(lengths) + ")"
-    
-    return f"{start_str}:{length_str}"
 
 
 def get_ddp_info():
